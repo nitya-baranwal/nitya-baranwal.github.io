cc7e40c34b12d15646862a8dd41e19762b7d72bb
nitya-nb <nityabaranwal1@gmail.com>
2022-10-28 13:22:48 +0530

    portfolio draft 1

---
+++ scripts/index.js
@@ -0,0 +1,66 @@
+
+function onClickMenu(){
+  document.getElementById("menu").classList.toggle("icon");
+  document.getElementById("nav").classList.toggle("change");
+}
+
+function removemenu(){
+
+  document.getElementById("menu").classList.toggle("icon");
+  document.getElementById("nav").classList.toggle("change");
+
+}
+
+
+
+
+consoleText(['Full Stack Web Developer!', 'Problem Solver'], 'text',['#86C232']);
+// rgb(220, 100, 2)
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  // window.setInterval(function() {
+  //   if (visible === true) {
+  //     consoleText.className = 'console-underscore hidden';
+  //     visible = false;
+
+  //   } else {
+  //     consoleText.className = 'console-underscore';
+
+  //     visible = true;
+  //   }
+  // }, 400)
+}
